4983e2c9c5f798ddbc6e75f63dbd36e418664e82
Dudu Popkhadze <dudupopkhadze@gmail.com>
2023-09-21 23:34:49 +0400

    feat: console.warn

--- src/runtime.js
+++ src/runtime.js
@@ -1,14 +1,17 @@
-((globalThis) => {
+((globalContext) => {
   function argsToMessage(...args) {
     return args.map((arg) => JSON.stringify(arg)).join(" ");
   }
 
-  globalThis.console = {
+  globalContext.console = {
     log: (...args) => {
-      Deno.core.print(`[out]: ${argsToMessage(...args)}\n`, false);
+      Deno.core.print(`[log]: ${argsToMessage(...args)}\n`, false);
     },
     error: (...args) => {
       Deno.core.print(`[err]: ${argsToMessage(...args)}\n`, true);
+    },
+    warn: (...args) => {
+      Deno.core.print(`[wrn]: ${argsToMessage(...args)}\n`, true);
     },
   };
 })(globalThis);
